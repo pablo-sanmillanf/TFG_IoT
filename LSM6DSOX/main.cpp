7ffbeb3c7402f18f4f607b6bdc86f4dfdde5054c
Pablo San Millán Fierro <pablo.sanmillanf@alumnos.upm.es>
2023-03-07 22:16:33 +0100

    LSM6DSOX test example improved
    Added to the previous example, the logic tocontrol gyroscope.

--- LSM6DSOX/main.cpp
+++ LSM6DSOX/main.cpp
@@ -8,13 +8,15 @@
 #include <signal.h>
 #include "i2c_master.h" // Module header
 //+-2g
-#define SCALE_RANGE 2
+#define ACC_SCALE_RANGE 2
+#define GYR_SCALE_RANGE 250
 #define ADR_LSM  0x6A
 
 static int finish = 0;
 
 float get_temperature(int16_t value);
-float convert(int16_t value, int scale_range);
+float convert_acc(int16_t value, int scale_range);
+float convert_gyr(int16_t value, int scale_range);
 void finisher(int signal);
 
 int main(){
@@ -24,7 +26,7 @@ int main(){
   
   I2C_Master::start(1);
   
-  int w_len = 2;
+  int w_len = 3;
   int r_len = 6;
   uint8_t data[w_len];
   uint8_t read_bytes[r_len];
@@ -38,22 +40,29 @@ int main(){
   printf("Reset status: %d\n", status);
   
   data[0] = 0x10;
-  data[1] = 0x40;
-  status =I2C_Master::write_msg(ADR_LSM, data,  2);
+  data[1] = 0x30;
+  data[2] = 0x30;
+  status =I2C_Master::write_msg(ADR_LSM, data,  3);
   
   while(!finish){
-  
     status =I2C_Master::read_msg(ADR_LSM, 0x20, read_bytes, 2);
   
     float temp = get_temperature(read_bytes[1] << 8 | read_bytes[0]);
+    printf("Temperature: %.1f\tRead status: %d\n", temp, status);
      
     status =I2C_Master::read_msg(ADR_LSM, 0x28, read_bytes, r_len);
   
-    float x_value = convert(read_bytes[1] << 8 | read_bytes[0], SCALE_RANGE);
-    float y_value = convert(read_bytes[3] << 8 | read_bytes[2], SCALE_RANGE);
-    float z_value = convert(read_bytes[5] << 8 | read_bytes[4], SCALE_RANGE);
+    float x_acc = convert_acc(read_bytes[1] << 8 | read_bytes[0], ACC_SCALE_RANGE);
+    float y_acc = convert_acc(read_bytes[3] << 8 | read_bytes[2], ACC_SCALE_RANGE);
+    float z_acc = convert_acc(read_bytes[5] << 8 | read_bytes[4], ACC_SCALE_RANGE);
 
-    printf("Read status: %d\t X Value: %.1f\t Y Value: %.1f\t Z Value: %.1f\t Temperature: %.1f\n", status, x_value, y_value, z_value, temp);
+    printf("Accelerometer:\t X: %.1f\t Y: %.1f\t Z: %.1f\tRead status: %d\n", x_acc, y_acc, z_acc, status);
+    
+    float x_gyr = convert_gyr(read_bytes[1] << 8 | read_bytes[0], GYR_SCALE_RANGE);
+    float y_gyr = convert_gyr(read_bytes[3] << 8 | read_bytes[2], GYR_SCALE_RANGE);
+    float z_gyr = convert_gyr(read_bytes[5] << 8 | read_bytes[4], GYR_SCALE_RANGE);
+
+    printf("Gyroscope:\t X: %.1f\t Y: %.1f\t Z: %.1f\tRead status: %d\n\n", x_gyr, y_gyr, z_gyr, status);
   
     sleep(2);
   }
@@ -66,7 +75,15 @@ float get_temperature(int16_t value){
   return (float)value / 256.0 + 25.0;
 }
 
-float convert(int16_t value, int scale_range){
+float convert_acc(int16_t value, int scale_range){
+  float result;
+  result = (float)value / 65536.0 * scale_range * 2;
+
+  return result;
+}
+
+
+float convert_gyr(int16_t value, int scale_range){
   float result;
   result = (float)value / 65536.0 * scale_range * 2;
 
